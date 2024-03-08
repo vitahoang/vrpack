3f4614ce6c1f0bc6b5eb91a0eed314681f288a31
VitaHoang <hvhung.oc@gmail.com>
2024-03-08 16:14:25 +0700

    docs: add readme

--- R/inference.R
+++ R/inference.R
@@ -13,6 +13,22 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
+
+
+#' Inference Confidence Interval for Proportion
+#'
+#' @param df The data frame
+#' @param res_var response value
+#' @param res_success success value response
+#'
+#' @return An inference vector contains inference information
+#' @export
+#'
+#' @examples
+#' data("atheism")
+#' us12 <- atheism %>%
+#'   filter(nationality == "United States" , atheism$year == "2012")
+#' inf_ci_proportion(us12, "response", "atheist")
 inf_ci_proportion <- function(df, res_var, res_success) {
   total_sample <- nrow(df) # total samples of the USA
   prop_sample <- nrow(df[df[[res_var]] == res_success,]) # total number of responses
@@ -21,9 +37,11 @@ inf_ci_proportion <- function(df, res_var, res_success) {
   # check CI condition
   if (p_hat*total_sample >=10 | (1-p_hat)*total_sample >=10){
     print("Condition met for CI")
-    inference(y = response, data = df, statistic = "proportion", type = "ci", method = "theoretical", success = res_success)
+    inf <- inference(y = response, data = df, statistic = "proportion", type = "ci", method = "theoretical", success = res_success)
+    return(inf)
   } else{
     print("Condition not met for CI")
+    return(NULL)
   }
 }
 
--- test.R
+++ test.R
@@ -7,4 +7,5 @@ us12 <- atheism %>%
   filter(nationality == "United States" , atheism$year == "2012")
 sum_us_12 <- summary(us12)
 
-inf_ci_proportion(us12, "response", "atheist")
+inf <- inf_ci_proportion(us12, "response", "atheist")
+print(inf[1])
